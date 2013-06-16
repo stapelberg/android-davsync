7be1b673c07806273e3008356258048a84c6760c
Alfred Toth <alfred.toth@inctoo.de>
2013-06-16 17:27:10 +0200

    new activity with list of pending uploads. possibillity to start manually upload of media files if an error occured

--- src/net/zekjur/davsync/DavSyncOpenHelper.java
+++ src/net/zekjur/davsync/DavSyncOpenHelper.java
@@ -11,7 +11,7 @@ import android.net.Uri;
 
 public class DavSyncOpenHelper extends SQLiteOpenHelper {
 	private static final String DATABASE_NAME = "davsync";
-	private static final int DATABASE_VERSION = 1;
+	private static final int DATABASE_VERSION = 2;
 
 	public DavSyncOpenHelper(Context context) {
 		super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -19,24 +19,38 @@ public class DavSyncOpenHelper extends SQLiteOpenHelper {
 
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		db.execSQL("CREATE TABLE IF NOT EXISTS sync_queue (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, uri STRING NOT NULL, not_before DATETIME, uploading BOOLEAN DEFAULT 0);");
+		db.execSQL("CREATE TABLE IF NOT EXISTS sync_queue (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, uri STRING NOT NULL, not_before DATETIME, uploading BOOLEAN DEFAULT 1);");
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		if (oldVersion == 1 && newVersion == 2) {
+			ArrayList<String> uris = getQueuedUris(db);
+			db.execSQL("DROP TABLE sync_queue;");
+			onCreate(db);
+			for (String uri : uris) {
+				queueUri(db, Uri.parse(uri));
+			}
+		}
 	}
 
+	public ArrayList<String> getQueuedUris() {
+		return getQueuedUris(null);
+	}
+	
 	/*
 	 * Returns an ArrayList of Uris which are queued and not already being
-	 * uploaded. Also marks all of them as being uploaded so that duplicate
-	 * network change events don’t upload the same Uris a lot of times.
+	 * uploaded.
 	 */
-	public ArrayList<String> getQueuedUris() {
+	public ArrayList<String> getQueuedUris(SQLiteDatabase db) {
 		ArrayList<String> result = new ArrayList<String>();
-		SQLiteDatabase database = getWritableDatabase();
+		SQLiteDatabase database = db;
+		if (db == null) {
+			database = getReadableDatabase();
+		}
 		database.beginTransaction();
 		try {
-			Cursor cursor = database.rawQuery("SELECT uri FROM sync_queue WHERE NOT uploading", null);
+			Cursor cursor = database.rawQuery("SELECT uri FROM sync_queue", null);
 			if (cursor.moveToFirst()) {
 				do {
 					result.add(cursor.getString(0));
@@ -46,24 +60,66 @@ public class DavSyncOpenHelper extends SQLiteOpenHelper {
 			if (cursor != null && !cursor.isClosed())
 				cursor.close();
 
-			database.execSQL("UPDATE sync_queue SET uploading = 1");
+			database.setTransactionSuccessful();
+		} finally {
+			database.endTransaction();
+			
+			if(db == null) {
+				database.close();
+			}
+		}
+		return result;
+	}
+	
+	public String getNextUri() {
+		String result = null;
+		SQLiteDatabase database = getReadableDatabase();
+		database.beginTransaction();
+		try {
+			Cursor cursor = database.rawQuery("SELECT uri FROM sync_queue WHERE uploading=1 LIMIT 1", null);
+			if (cursor.moveToFirst()) {
+				result = cursor.getString(0);
+			}
+
+			if (cursor != null && !cursor.isClosed())
+				cursor.close();
 
 			database.setTransactionSuccessful();
 		} finally {
 			database.endTransaction();
+			database.close();
 		}
 		return result;
 	}
+	
+	public void setUploading(String uri, int uploadingValue) {
+		SQLiteDatabase database = getWritableDatabase();
+		ContentValues values = new ContentValues();
+		values.put("uploading", uploadingValue);
+		database.update("sync_queue", values, "uri=?", new String[]{uri});
+		database.close();
+	}
 
 	public void queueUri(Uri uri) {
-		SQLiteDatabase database = getWritableDatabase();
+		queueUri(null, uri);
+	}
+	
+	public void queueUri(SQLiteDatabase db , Uri uri) {
+		SQLiteDatabase database = db;
+		if (db == null) {
+			database = getWritableDatabase();
+		}
 		ContentValues values = new ContentValues();
 		values.put("uri", uri.toString());
 		database.insertOrThrow("sync_queue", null, values);
+		if (db == null) {
+			database.close();
+		}
 	}
 
 	public void removeUriFromQueue(String uri) {
 		SQLiteDatabase database = getWritableDatabase();
 		database.delete("sync_queue", "uri = ?", new String[] { uri });
+		database.close();
 	}
 }
--- src/net/zekjur/davsync/NetworkReceiver.java
+++ src/net/zekjur/davsync/NetworkReceiver.java
@@ -1,14 +1,11 @@
 package net.zekjur.davsync;
 
-import java.util.ArrayList;
-
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.Uri;
 import android.util.Log;
 
 public class NetworkReceiver extends BroadcastReceiver {
@@ -40,19 +37,8 @@ public class NetworkReceiver extends BroadcastReceiver {
 
 		Log.d("davsync", "Checking whether pictures need to be synced");
 
-		// XXX: It doesn’t really feel right to do this blockingly in a
-		// BroadcastReceiver, but I was unable to find whether this is the right
-		// way or whether there is a better one.
-
-		DavSyncOpenHelper helper = new DavSyncOpenHelper(context);
-		ArrayList<String> uris = helper.getQueuedUris();
-		for (String uri : uris) {
-			Intent ulIntent = new Intent(context, UploadService.class);
-			// evtl: mapintent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-
-			ulIntent.putExtra(Intent.EXTRA_STREAM, Uri.parse(uri));
-			context.startService(ulIntent);
-		}
+		Intent ulIntent = new Intent(context, UploadService.class);
+		context.startService(ulIntent);
 	}
 
 }
